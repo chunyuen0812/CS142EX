ffb819580e5e78bb8d475d6c81c17bf301e2e2cb
Jason <jason.tsang@fcc-analytics.com>
2023-09-06 19:09:54 +0800

    project 2 updated

---
+++ project2/cs142-template-processor.js
@@ -0,0 +1,11 @@
+'use strict';
+function Cs142TemplateProcessor(template){
+    this.template = template;
+}
+
+Cs142TemplateProcessor.prototype.fillIn = function(dictionary){
+
+
+    return dictionary;
+
+};
